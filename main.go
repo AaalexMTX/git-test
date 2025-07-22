0d668e4cb3e2050fd9a6da4be4fccfb9142eff1b
yongxiang.chen <yongxiang.chen@bluefocus.com>
2025-07-22 16:16:45 +0800

    conflict reflash

--- main.go
+++ main.go
@@ -11,7 +11,11 @@ func main() {
 
 	fmt.Println("main 1")
 
+<<<<<<< HEAD
 	fmt.Println("feature/test 2")
 
 	fmt.Println("feature/test 3")
+=======
+	fmt.Println("feature/test2 1")
+>>>>>>> 782451b (f/t2 c 1)
 }
