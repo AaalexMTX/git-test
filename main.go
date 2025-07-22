976d800adb06091205a3b831a8d8864a4b8c28b6
yongxiang.chen <yongxiang.chen@bluefocus.com>
2025-07-22 16:20:31 +0800

    qa refresh

--- main.go
+++ main.go
@@ -8,4 +8,11 @@ func main() {
 	fmt.Println("hello world")
 
 	fmt.Println("feature/test 1")
+
+	fmt.Println("main 1")
+
+	fmt.Println("feature/test 2")
+
+	fmt.Println("feature/test 3")
+	fmt.Println("feature/test2 1")
 }
